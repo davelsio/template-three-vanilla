6a07d03114ff10e206cb15a8f43da1623045cc9d
David Velasco <davvelsan@gmail.com>
2022-08-02 19:16:10 +0200

    feat: restore navigation menus, consistent with the r3f template

--- src/pages/threejs-journey/threejs-journey.tsx
+++ src/pages/threejs-journey/threejs-journey.tsx
@@ -2,6 +2,8 @@ import './threejs-journey.scss';
 import './experience/styles/tweak-pane.scss';
 
 import React from 'react';
+import { PerspectiveCamera } from 'three';
+import { OrbitControls } from 'three-stdlib';
 
 import { useEffectOnce } from '@/hooks';
 
@@ -10,6 +12,9 @@ import { Experience } from './experience';
 
 const Canvas: React.FC = () => {
   const rootRef = React.useRef<HTMLDivElement>(null);
+  const cameraRef = React.useRef<PerspectiveCamera | null>(null);
+  const controlsRef = React.useRef<OrbitControls | null>(null);
+
   const [loaded, setLoaded] = React.useState(false);
 
   useEffectOnce(() => {
@@ -17,7 +22,11 @@ const Canvas: React.FC = () => {
     let experience: Experience;
     if (root) {
       experience = new Experience(root);
-      experience.onLoad(() => setLoaded(true));
+      experience.onLoad(() => {
+        cameraRef.current = experience.api.camera;
+        controlsRef.current = experience.api.controls;
+        setLoaded(true);
+      });
     }
 
     return () => {
@@ -27,7 +36,7 @@ const Canvas: React.FC = () => {
 
   return (
     <>
-      {loaded && <Navigation />}
+      {loaded && <Navigation cameraRef={cameraRef} controlsRef={controlsRef} />}
       <div ref={rootRef} className="webgl__wrapper" />
     </>
   );
