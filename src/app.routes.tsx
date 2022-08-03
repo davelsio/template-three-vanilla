d00d0b24a2a6ec089a8a59bce25ff2b3163c567d
David Velasco <davvelsan@gmail.com>
2022-08-03 16:41:55 +0200

    feat: lazy load experience

--- src/app.routes.tsx
+++ src/app.routes.tsx
@@ -1,14 +1,20 @@
+import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './pages/home/home.routes';
-import ThreejsJourney from './pages/threejs-journey/threejs-journey.routes';
+
+const ThreejsJourney = React.lazy(
+  () => import('./pages/threejs-journey/threejs-journey.routes')
+);
 
 const MainRoutes: React.FC = () => {
   return (
-    <Routes>
-      <Route path="*" element={<Home />} />
-      <Route path="/threejs-journey/*" element={<ThreejsJourney />} />
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/threejs-journey/*" element={<ThreejsJourney />} />
+      </Routes>
+    </Suspense>
   );
 };
 
